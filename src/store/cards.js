ab0b5899977ce47f6da688209937cf613f1cbfb2
ryanechternacht <ryan@echternacht.org>
2020-01-08 13:36:00 -0500

    Add more cards

--- src/store/cards.js
+++ src/store/cards.js
@@ -1,9 +1,50 @@
 export default {
   cards: [
     {
+      name: "colonizer training camp",
+      id: "1",
+      tags: [
+        {
+          name: "building tag",
+          value: "jove"
+        },
+        {
+          name: "building tag",
+          value: "building"
+        },
+        {
+          name: "type",
+          value: "automated"
+        },
+        {
+          name: "vps",
+          value: 2
+        }
+      ],
+      cost: 8
+    },
+    {
+      name: "asteroid mining consortium",
+      id: "2",
+      tags: [
+        {
+          name: "building tag",
+          value: "jove"
+        },
+        {
+          name: "vps",
+          value: 1
+        },
+        {
+          name: "type",
+          value: "automated"
+        }
+      ],
+      cost: 13
+    },
+    {
       name: "deep well heating",
       id: "3",
-      type: "automated",
       tags: [
         {
           name: "building tag",
@@ -14,12 +55,67 @@ export default {
           value: "building"
         },
         {
-          name: "raises temperature",
-          value: true
+          name: "type",
+          value: "automated"
+        }
+      ],
+      cost: 13
+    },
+    {
+      name: "cloud seeding",
+      id: "4",
+      tags: [],
+      cost: 11
+    },
+    {
+      name: "search for life",
+      id: "5",
+      tags: [
+        {
+          name: "building tag",
+          value: "science"
         },
         {
-          name: "provides power income",
-          value: 1
+          name: "type",
+          value: "active"
+        }
+      ],
+      cost: 3
+    },
+    {
+      name: "inventors guild",
+      id: "6",
+      tags: [
+        {
+          name: "building tag",
+          value: "science"
+        },
+        {
+          name: "action",
+          value: "true"
+        },
+        {
+          name: "type",
+          value: "active"
+        }
+      ],
+      cost: 9
+    },
+    {
+      name: "martial rails",
+      id: "7",
+      tags: [
+        {
+          name: "building tag",
+          value: "building"
+        },
+        {
+          name: "action",
+          value: "true"
+        },
+        {
+          name: "type",
+          value: "active"
         }
       ],
       cost: 13
@@ -27,7 +123,6 @@ export default {
     {
       name: "capital",
       id: "8",
-      type: "automated",
       tags: [
         {
           name: "building tag",
@@ -38,42 +133,95 @@ export default {
           value: "building"
         },
         {
-          name: "places city",
-          value: true
-        },
-        {
-          name: "costs power income",
-          value: 2
-        },
-        {
-          name: "provides megacredit income",
-          value: 5
+          name: "type",
+          value: "automated"
         }
-      ]
+      ],
+      cost: 26
     },
     {
-      name: "martial rails",
-      id: "7",
-      type: "active",
+      name: "asteroid",
+      id: "9",
       tags: [
         {
           name: "building tag",
-          value: "building"
+          value: "event"
         },
         {
-          name: "is action",
-          value: "true"
+          name: "building tag",
+          value: "space"
         },
         {
-          name: "spend power",
-          value: true
+          name: "type",
+          value: "event"
         }
-      ]
+      ],
+      cost: 14
+    },
+    {
+      name: "comet",
+      id: "10",
+      tags: [
+        {
+          name: "building tag",
+          value: "event"
+        },
+        {
+          name: "building tag",
+          value: "space"
+        },
+        {
+          name: "type",
+          value: "event"
+        }
+      ],
+      cost: 21
+    },
+    {
+      name: "big asteroid",
+      id: "11",
+      tags: [
+        {
+          name: "building tag",
+          value: "event"
+        },
+        {
+          name: "building tag",
+          value: "space"
+        },
+        {
+          name: "type",
+          value: "event"
+        }
+      ],
+      cost: 27
+    },
+    {
+      name: "water import from europa",
+      id: "12",
+      tags: [
+        {
+          name: "building tag",
+          value: "jove"
+        },
+        {
+          name: "type",
+          value: "active"
+        },
+        {
+          name: "action",
+          value: true
+        },
+        {
+          name: "vps",
+          value: "*"
+        }
+      ],
+      cost: 27
     },
     {
       name: "release of inert gases",
       id: "36",
-      type: "event",
       tags: [
         {
           name: "building tag",
@@ -82,6 +230,10 @@ export default {
         {
           name: "raises TR",
           value: 2
+        },
+        {
+          name: "type",
+          value: "event"
         }
       ]
     }

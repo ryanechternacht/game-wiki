dd5c2f2e5e0e813c2d3cc02f6465926d78d487ab
ryanechternacht <ryan@echternacht.org>
2020-01-09 08:49:56 -0500

    Add a few faq items to vuex

---
+++ src/store/faqs.js
@@ -0,0 +1,25 @@
+export default {
+  faqs: [
+    {
+      title: "How many actions can I take in a generation?",
+      body:
+        "As many as you want. You keep taking turns until you pass a turn without taking any actions.",
+      tags: ["general"]
+    },
+    {
+      title: "What are the best expansions?",
+      body: "1) Prelude, 2) Colonies, 3) Turmoil, 4) Venus",
+      tags: ["general"]
+    },
+    {
+      title: "Is a chairman part of a party?",
+      body: "Who can know?",
+      tags: ["turmoil", "chairman", "party"]
+    },
+    {
+      title: "Can I use a <?> tag during a new government bonus",
+      body: "No. You can only use <?> tags while taking an action",
+      tags: ["turmoil", "?-tag"]
+    }
+  ]
+};
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,14 @@ import Vue from "vue";
 import Vuex from "vuex";
 
 import cardList from "./cards.js";
+import faqList from "./faq.js";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-    cards: cardList.cards
+    cards: cardList.cards,
+    faq: faqList.faq
   },
   mutations: {},
   actions: {},

cf1030a3575b2bca53bbeffb067929df28b2ce61
ryanechternacht <ryan@echternacht.org>
2020-01-27 08:56:44 -0500

    Cleanup

--- src/components/layout/TheFooter.vue
+++ src/components/layout/TheFooter.vue
@@ -8,23 +8,13 @@
 
 <script>
 export default {
-  created() {
-    this.updateCurrentArea();
-  },
-  watch: {
-    $route: "updateCurrentArea"
-  },
-  methods: {
-    updateCurrentArea() {
-      this.area = this.$route.matched.length && this.$route.matched[0].name;
-    }
-  }
+  name: "the-footer"
 };
 </script>
 
 <style scoped>
 .footer {
-  background-color: #f8f8f8;
+  background-color: #f8f8fa;
   margin-top: 1rem;
   padding-top: 3rem;
   padding-bottom: 3rem;

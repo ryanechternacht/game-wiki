a249531029e6ad2d57a8d0124e61263d3962cb64
ryanechternacht <ryan@echternacht.org>
2020-03-18 23:03:17 -0400

    Move cards over to using our backend api

--- src/store/modules/cards.js
+++ src/store/modules/cards.js
@@ -1,12 +1,32 @@
-import cardList from "../data/cards.js";
+import axios from "axios";
 
 export default {
   namespaced: true,
   state: {
-    cards: cardList.cards
+    cards: null
   },
-  getters: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+  getters: {
+    getCards: state => {
+      return state.cards ? state.cards : [];
+    }
+  },
+  mutations: {
+    setCards(state, cards) {
+      state.cards = cards;
+    }
+  },
+  actions: {
+    fetchCards({ commit, getters }) {
+      if (getters.getCards.length) {
+        return;
+      }
+
+      axios({
+        url: "http://localhost:8890/cards",
+        headers: { "Content-Type": "application/json" }
+      }).then(response => {
+        commit("setCards", response.data);
+      });
+    }
+  }
 };
